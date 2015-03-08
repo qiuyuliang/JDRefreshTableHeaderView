33465a196adec8175ed6aef66343aac845e3bf84
qiuyuliang <iqiuyuliang@163.com>
2015-03-08 12:29:46 +0800

    Initial Commit

---
+++ JDRefreshTableHeaderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDRefreshTableHeaderView
+//
+//  Created by 邱 育良 on 15/3/8.
+//  Copyright (c) 2015年 Qiu Yuliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDRefreshTableHeaderView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JDRefreshTableHeaderView
+//
+//  Created by 邱 育良 on 15/3/8.
+//  Copyright (c) 2015年 Qiu Yuliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
